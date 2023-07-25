83ad9c044468ad2838bb9a23d39a6ec93f87d062
Batandwa Mgutsi <b.mgutsi@gmail.com>
2023-07-25 13:53:41 +0200

    Feat: Adds frontend

--- src/blockchainBulls_frontend/src/index.js
+++ src/blockchainBulls_frontend/src/index.js
@@ -1,5 +1,40 @@
 import { blockchainBulls_backend } from "../../declarations/blockchainBulls_backend";
 
+const bids = [];
+
+async function loadAuctions() {
+
+}
+
+async function refreshBidsForAuction() {
+  const container = document.getElementById("bids-list");
+  container.innerText = "";
+
+  for(const bid of bids) {
+    container.innerHTML = `${container.innerHTML}\n<a class="list-group-item list-group-item-action" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">${bid.bidderName} bids R${bid.amount}</a>`
+  }
+}
+
+async function addAuction() {
+
+}
+
+async function placeBid() {
+  const bidderName = prompt("What is your bidder name?");
+  const amount = Math.round(Math.random()*200);
+
+  // ---- place bid
+
+  bids.push({
+    bidderName,
+    amount,
+  });
+
+  refreshBidsForAuction();
+}
+
+async function reloadPage() {}
+
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const button = e.target.querySelector("button");
